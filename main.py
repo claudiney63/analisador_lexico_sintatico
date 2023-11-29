112732fbf135169d603b5ddb4d11e3957fbcf36d
claudiney63 <claudineyryan75@gmail.com>
2023-11-29 11:36:17 -0300

    divisão de arquivos e exemplos

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
 from antlr4 import *
-from SimpAlgLexer import SimpAlgLexer
-from SimpAlgParser import SimpAlgParser
+from gen.SimpAlgLexer import SimpAlgLexer
+from gen.SimpAlgParser import SimpAlgParser
 
-def test_simpalg(input_code):
-    input_stream = InputStream(input_code)
-    lexer = SimpAlgLexer(input_stream)
-    tokens = CommonTokenStream(lexer)
-    parser = SimpAlgParser(tokens)
-    tree = parser.programa()
+input_stream = FileStream("ex.txt")
+lexer = SimpAlgLexer(input_stream)
+tokens = CommonTokenStream(lexer)
+parser = SimpAlgParser(tokens)
+tree = parser.programa()
 
     # Adicione lógica adicional para testar a árvore sintática, se necessário
 
-    print(tree.toStringTree(parser))  # Imprime a árvore de análise sintática
+print(tree.toStringTree(parser))  # Imprime a árvore de análise sintática
 
 # Exemplo de código para teste
-test_simpalg("""
-var {
-    int a, b;
-}
-program {
-    a = 10;
-    b = 5 + a;
-    print("Resultado: ", b);
-}
-""")
+
